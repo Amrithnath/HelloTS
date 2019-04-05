13ead2164b4ec34ac40728ec0d753c340af2cfac
Amrith <arjun.amrith@gmail.com>
2019-04-05 22:32:52 +0530

    Object instance of class stuff

---
+++ custom_inline_annotation.js
@@ -0,0 +1,12 @@
+class Point {
+    draw() {
+        console.log("X: " + this.x, "Y :" + this.y);
+    }
+    getDistance(another) {
+        //..
+    }
+}
+let point = new Point();
+point.x = 1;
+point.x = 2;
+point.draw();
--- custom_inline_annotation.ts
+++ custom_inline_annotation.ts
@@ -1,10 +1,15 @@
-class Point{
+class Point{//.class
     x:number;
     y:number;
     draw(){
-        //..
+        console.log("X: "+this.x+",Y :" +this.y);
     }
     getDistance(another:Point){
-        //..
+        console.log(this.x-this.y)
     }
 }
+
+let point = new Point();//object (instance of class)
+point.x=1;
+point.x=2;
+point.draw();
