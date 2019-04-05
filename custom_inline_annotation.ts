40fc35129ba02c7f2d367ae0e19a9693d1c9ee82
Amrith <arjun.amrith@gmail.com>
2019-04-05 22:45:34 +0530

    Properties to set and get pricate fields addede:

--- custom_inline_annotation.ts
+++ custom_inline_annotation.ts
@@ -1,18 +1,32 @@
 class Point{//.class
-    x:number;
-    y:number;
-    constructor(x?:number,y?:number){//'?' makes the argument optional, Important *** if one of the arguments is optional all must be optional TS rule***
-        this.x=x;
-        this.y=y;
-
+    // private x:number;
+    // private y:number;//private cant be accessed outside class unless through constructor(default is Public) Public Protected and private
+    constructor(private x?:number,private y?:number){
+        //'?' makes the argument optional, Important *** if one of the arguments is optional all must be optional TS rule***
     }
     draw(){
         console.log("X: "+this.x+",Y :" +this.y);
     }
-    getDistance(another:Point){
+    private getDistance(another:Point){
         console.log(this.x-this.y)
     }
+    get X(){
+        return this.x;
+    }
+    get Y(){
+        return this.y;
+    }
+    set X(value){
+        if(value <0){
+            throw new Error('value cannot be less than 0.');
+        }
+        else{
+            this.x=value;
+        }
+    }
 }
-
 let point = new Point(1,2);//object (instance of class)
+let x = point.X;
+let y = point.Y;
+point.X=10;
 point.draw();
